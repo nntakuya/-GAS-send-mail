f8f656e147b090e2dc92534ea3e7a1ecd09c5a51
nntakuya <uv.nkmt.yii@gmail.com>
2020-10-10 16:29:20 +0900

    title, content取得関数追加

--- src/Code.ts
+++ src/Code.ts
@@ -1,6 +1,9 @@
 // Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
 function main() {
   const addresses = getAdresses();
+
+  const title = getTitle();
+  const content = getContent();
 }
 
 function getAdresses() {
@@ -10,4 +13,16 @@ function getAdresses() {
   const addressCounter = addressSheet.getRange('C:C').getValues().filter(String).length - tabNameCount;
 
   return addressSheet.getRange(5, 3, addressCounter).getValues();
+}
+
+function getTitle() {
+  const templateSheet = SpreadsheetApp.getActive().getSheetByName('メールテンプレート');
+
+  return templateSheet.getRange('C3').getValue();
+}
+
+function getContent() {
+  const templateSheet = SpreadsheetApp.getActive().getSheetByName('メールテンプレート');
+
+  return templateSheet.getRange('C4').getValue();
 }
