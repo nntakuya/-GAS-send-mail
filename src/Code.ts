cd63c0176f92e008fce3fc4f727e3f7f5bdc8474
nntakuya <uv.nkmt.yii@gmail.com>
2020-10-10 17:09:29 +0900

    送信処理追加

--- src/Code.ts
+++ src/Code.ts
@@ -1,9 +1,12 @@
 // Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
 function main() {
   const addresses = getAdresses();
-
   const title = getTitle();
   const content = getContent();
+
+  addresses.forEach(address => {
+    sender(address[0], title, content);
+  });
 }
 
 function getAdresses() {
@@ -15,14 +18,25 @@ function getAdresses() {
   return addressSheet.getRange(5, 3, addressCounter).getValues();
 }
 
+// todo: 下記の関数を一つにまとめる
 function getTitle() {
   const templateSheet = SpreadsheetApp.getActive().getSheetByName('メールテンプレート');
 
-  return templateSheet.getRange('C3').getValue();
+  return templateSheet.getRange('C6').getValue();
 }
 
 function getContent() {
   const templateSheet = SpreadsheetApp.getActive().getSheetByName('メールテンプレート');
 
-  return templateSheet.getRange('C4').getValue();
+  return templateSheet.getRange('C7').getValue();
+}
+
+// function getFromAdress() {
+//   const templateSheet = SpreadsheetApp.getActive().getSheetByName('メールテンプレート');
+
+//   return templateSheet.getRange('C2').getValue();
+// }
+
+function sender(address: String, title: String, body: String) {
+  GmailApp.sendEmail(address, title, body);
 }
