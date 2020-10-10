6867271cc2f1b2d521f5300ca4ec73db22e5afa0
nntakuya <uv.nkmt.yii@gmail.com>
2020-10-10 16:14:47 +0900

    address取得関数追加

--- src/Code.ts
+++ src/Code.ts
@@ -1,3 +1,13 @@
-function myFunction() {
-  
+// Compiled using ts2gas 3.6.3 (TypeScript 3.9.7)
+function main() {
+  const addresses = getAdresses();
+}
+
+function getAdresses() {
+  const addressSheet = SpreadsheetApp.getActive().getSheetByName('リスト');
+
+  const tabNameCount = 1;
+  const addressCounter = addressSheet.getRange('C:C').getValues().filter(String).length - tabNameCount;
+
+  return addressSheet.getRange(5, 3, addressCounter).getValues();
 }
